93833b46efd537869c0755b94f729caf57d2ab2e
LeMinhDang <leminhdang0701@gmail.com>
2019-02-22 01:56:39 -0800

    Day 8

---
+++ fitness/customer.php
@@ -0,0 +1,10 @@
+<?php
+  $hostname = 'localhost';
+  $username = 'root';
+  $password = '';
+  $database = 'fitness';
+
+    //opening a fann_get_total_connections
+    $conn = new mysqli($hostname,$username,$password,$database)
+    if ($conn)
+ ?>
